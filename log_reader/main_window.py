cef66247cf39e6d08556a21c356f20bb2fcdfbad
asmodeus94 <18900555+asmodeus94@users.noreply.github.com>
2026-07-20 14:58:53 +0000

    style: adjust QProgressDialog layout spacing
    * Added minimum width and height to QProgressDialog in QSS
    * Added top margin to the 'Cancel' button in QProgressDialog to properly separate it from the progress bar when native styles are overridden by application themes

--- log_reader/main_window.py
+++ log_reader/main_window.py
@@ -40,7 +40,7 @@ from .indexer import LineIndexer, IndexEntry
 from .filter_engine import FilterEngine
 from .edit_buffer import EditBuffer
 from .workers import IndexerWorker, FilterWorker, SaveWorker
-from .widgets import LogPlainTextEdit, SettingsDialog, SearchResultsModel, MiniMap, FormatDialog, ExpandingLineEdit
+from .widgets import LogPlainTextEdit, SettingsDialog, SearchResultsModel, MiniMap, FormatDialog
 from .ui.ui_main_window import Ui_LogViewerWindow
 
 from .log_tab import LogTab
@@ -451,6 +451,12 @@ class LogViewerWindow(QMainWindow):
             }}
             QProgressDialog {{
                 background-color: {t["bg_panel"]};
+                min-width: 300px;
+                min-height: 120px;
+            }}
+            QProgressDialog > QPushButton {{
+                margin-top: 15px;
+                min-width: 80px;
             }}
             QDialog {{
                 background-color: {t["bg_panel"]};
@@ -483,7 +489,8 @@ class LogViewerWindow(QMainWindow):
 
         self.lbl_search = QLabel(self.t("lbl_search"))
         toolbar.addWidget(self.lbl_search)
-        self.search_entry = ExpandingLineEdit()
+        self.search_entry = QLineEdit()
+        self.search_entry.setMaximumWidth(250)
         self.search_entry.returnPressed.connect(self.cmd_find_next)
         toolbar.addWidget(self.search_entry)
 
@@ -510,7 +517,8 @@ class LogViewerWindow(QMainWindow):
 
         self.lbl_filter = QLabel(self.t("lbl_filter"))
         toolbar.addWidget(self.lbl_filter)
-        self.filter_entry = ExpandingLineEdit()
+        self.filter_entry = QLineEdit()
+        self.filter_entry.setMaximumWidth(250)
         self.filter_entry.returnPressed.connect(self.cmd_apply_filter)
         toolbar.addWidget(self.filter_entry)
 
